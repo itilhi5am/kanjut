39a3dd13cc2785c7fd6802a015aee327887ae5a9
itilhi5am <daffaindana578@gmail.com>
2026-01-09 17:23:29 +0700

    Add files via upload

--- api1.js
+++ api1.js
@@ -32,7 +32,7 @@ app.get('/api', (req, res) => {
     console.log(`Running ${method}: ${scriptPath}`);
 
     // GUNAKAN spawn agar bisa kill 1 group (-pid7)
-    const child = spawn("node", [scriptPath, host, time, 120, 5, "proxy.txt"], {
+    const child = spawn("node", [scriptPath, host, time, 90, 5, "proxy.txt"], {
         detached: true, // MEMBUAT PROCESS GROUP
         stdio: "ignore"
     });
